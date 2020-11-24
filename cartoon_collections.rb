f67ddacc82943fbf4c56d7f1cf88f56d8c3f0c7f
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-24 01:19:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with_index(1){|name, index| puts "#{index}. #{name}"}
 end
 
-def summon_captain_planet(array)
-  array.map{|shouts| "#{shouts.capitalize}!"}
+def summon_captain_planet(summon)
+  summon.map{|shouts| "#{shouts.capitalize}!"}
 end
 
 def long_planeteer_calls(calls)
