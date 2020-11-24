0c9432130d9dd504dbd0ce3a80862fac4fcbcf44
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-24 01:36:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def long_planeteer_calls(calls)
   calls.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
